16fa428af7b6c9de845975f9afa2a65c1369e04b
Mateo Loholaberry <mateoloholaberry@gmail.com>
2022-12-30 12:05:43 -0300

    sacando bindings que no funcionaban de crearVerProducto

--- app_de_productos/app_de_productos/ViewModel/CrearVerProductoViewModel.cs
+++ app_de_productos/app_de_productos/ViewModel/CrearVerProductoViewModel.cs
@@ -5,26 +5,11 @@ namespace app_de_productos.ViewModel
 {
     public partial class CrearVerProductoViewModel : ObservableObject
     {
-        [ObservableProperty]
-        string nombre;
-
-        [ObservableProperty]
-        string marca;
-
-        [ObservableProperty]
-        string precio;
-
-        [ObservableProperty]
-        string descripcion;
-
-        [ObservableProperty]
-        string imagen;
-
 
         [RelayCommand]
         async void CargarProducto()
         {
-            // Cargar producto a la base de datos con las propiedades de arriba.
+            // Cargar o modificar producto a la base de datos
 
             await Shell.Current.GoToAsync($"//{nameof(MainPage)}");
         }
